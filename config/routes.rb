b847fdb25a33fe1e3d65e6f2a2fdbb8ff56a78c1
AntoMonti <a.matviishyn@hotmail.com>
2020-01-17 15:08:24 +0200

    add resources :projects

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'about' => 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :projects
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   root 'home#index'
 end
