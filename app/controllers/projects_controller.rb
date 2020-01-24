26384b8a63e8f1d85bece450346e3117d0583f01
AntoMonti <a.matviishyn@hotmail.com>
2020-01-24 09:01:14 +0200

    rectified syntax errors

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create] 
+  before_action :authenticate_artist!, :only => [:new, :create] 
 
   def index
     @projects = Project.all
